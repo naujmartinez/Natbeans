514d29d75f1d7eeedbd619eb512e358bd2ea6faa
naujmartinez <juan.martinez@alumno.uttehuacan.edu.mx>
2017-05-20 11:11:54 -0500

    Primer metodo

---
+++ ProyectoMiGit/src/proyectomigit/ProyectoMiGit.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectomigit;
+
+/**
+ *
+ * @author juanm
+ */
+public class ProyectoMiGit {
+public void Hola(){
+    System.out.println("Hola mundo");
+}
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ProyectoMiGit git = new ProyectoMiGit();
+        git.Hola();
+        
+    }
+    
+}
